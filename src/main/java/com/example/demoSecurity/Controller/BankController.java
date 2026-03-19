42433cb365f85c2d6b4288d4ebdef55d97e8fde2
Hrishikesh Damakale <hrishikeshdamakale@gmail.com>
2026-03-19 17:50:16 +0530

    Role Based Authentication

--- src/main/java/com/example/demoSecurity/Controller/BankController.java
+++ src/main/java/com/example/demoSecurity/Controller/BankController.java
@@ -3,6 +3,7 @@ package com.example.demoSecurity.Controller;
 
 import com.example.demoSecurity.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class BankController  {
 
     @Autowired
     private AccountService accountService;
+
+    //setting role based autherization
+    //to implement this will enforce the authorisation in the service class
 
 
     @GetMapping("/balance")
--- src/main/java/com/example/demoSecurity/Service/AccountService.java
+++ src/main/java/com/example/demoSecurity/Service/AccountService.java
@@ -2,15 +2,21 @@ package com.example.demoSecurity.Service;
 
 
 import com.example.demoSecurity.Controller.BankController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
+    //here this states that the to see bal user need only authenticated
+    @PreAuthorize("isAuthenticated()")
     public String getBalance(){
         return "Your Account Balance is = 80,000";
     }
 
+
+    //here it is saying that user authentication aS WELL as authorisation as admin
+    @PreAuthorize("hasRole('ADMIN')")
     public String closeAccount(){
         return "Your account has been Closed";
     }
